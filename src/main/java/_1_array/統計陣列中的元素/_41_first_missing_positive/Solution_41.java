3b20536c538a71a32688f1615c9b2d0c55bc283d
Henry.Liao <Henry.Liao@skylinetw.com>
2023-12-01 15:36:46 +0800

    feat(_1_array.統計陣列中的元素): _41_first_missing_positive

---
+++ src/main/java/_1_array/統計陣列中的元素/_41_first_missing_positive/Solution_41.java
@@ -0,0 +1,6 @@
+package _1_array.統計陣列中的元素._41_first_missing_positive;
+
+// https://leetcode.com/problems/first-missing-positive
+public interface Solution_41 {
+    int firstMissingPositive(int[] nums);
+}
