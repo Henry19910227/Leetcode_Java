8625b260ea832f00426b15eb869819263d975815
henry <toyokoyo199@gmail.com>
2023-12-03 03:32:08 +0800

    feat(_1_array.統計陣列中的元素): _274_h_Index

---
+++ src/main/java/_1_array/統計陣列中的元素/_274_h_Index/Solution_274.java
@@ -0,0 +1,7 @@
+package _1_array.統計陣列中的元素._274_h_Index;
+
+// https://leetcode.com/problems/h-index/description/
+// https://subjectguides.uwaterloo.ca/calculate-academic-footprint/YourHIndex
+public interface Solution_274 {
+    int hIndex(int[] citations);
+}
