8e3a80bb7d7a7179567c4722a8caed9e76a804ee
Henry.Liao <Henry.Liao@skylinetw.com>
2023-12-05 12:31:34 +0800

    feat(_1_array.二維陣列及滾動陣列): _598_range_addition_II

---
+++ src/main/java/_1_array/二維陣列及滾動陣列/_598_range_addition_II/Solution_598.java
@@ -0,0 +1,5 @@
+package _1_array.二維陣列及滾動陣列._598_range_addition_II;
+
+public interface Solution_598 {
+    int maxCount(int m, int n, int[][] ops);
+}
---
+++ src/test/java/_1_array/二維陣列及滾動陣列/_598_range_addition_II/SolutionTest.java
@@ -0,0 +1,14 @@
+package _1_array.二維陣列及滾動陣列._598_range_addition_II;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    @Test
+    void test() {
+        int[][] ops = {{2,2},{3,3},{3,3},{3,3},{2,2},{3,3},{3,3},{3,3},{3,3}};
+        Solution_598 solution = new Solution();
+        int output = solution.maxCount(3, 3, ops);
+        assertEquals(4 ,output);
+    }
+}
