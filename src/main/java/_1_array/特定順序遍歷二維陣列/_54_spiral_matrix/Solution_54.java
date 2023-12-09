bfda244c7838f0d7010ea4d3bdf9073f6636b118
henry <toyokoyo199@gmail.com>
2023-12-10 01:41:24 +0800

    feat(_1_array.特定順序遍歷二維陣列): _54_spiral_matrix;

---
+++ src/main/java/_1_array/特定順序遍歷二維陣列/_54_spiral_matrix/Solution_54.java
@@ -0,0 +1,8 @@
+package _1_array.特定順序遍歷二維陣列._54_spiral_matrix;
+
+import java.util.List;
+
+// https://leetcode.com/problems/spiral-matrix/
+public interface Solution_54 {
+    List<Integer> spiralOrder(int[][] matrix);
+}
